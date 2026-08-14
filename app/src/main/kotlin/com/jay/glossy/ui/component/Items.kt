ad2973f69557f92cdf6b1d9aeef1d610791d363e
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-08-14 16:04:28 +0530

    Update project name from Glossy to Metrolist

--- app/src/main/kotlin/com/jay/glossy/ui/component/Items.kt
+++ app/src/main/kotlin/com/jay/glossy/ui/component/Items.kt
@@ -1,5 +1,5 @@
 /**
- * Glossy Project (C) 2026
+ * Metrolist Project (C) 2026
  * Licensed under GPL-3.0 | See git history for contributors
  * 
  * Optimized for minimal recomposition during navigation
