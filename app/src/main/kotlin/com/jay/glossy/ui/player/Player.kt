78020b5b5abf8aa9c30d77a6b75616a551ccdb5b
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-08-21 07:19:01 +0530

    Add Surface import to Player.kt

--- app/src/main/kotlin/com/jay/glossy/ui/player/Player.kt
+++ app/src/main/kotlin/com/jay/glossy/ui/player/Player.kt
@@ -75,6 +75,7 @@ import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
