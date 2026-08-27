a6cec33e73fcbda06de8a0bf53582ab56d97ff42
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-08-28 00:19:10 +0530

    Update Palette import to use com.kmpalette package

--- app/src/main/kotlin/com/jay/glossy/ui/component/LyricsComponents.kt
+++ app/src/main/kotlin/com/jay/glossy/ui/component/LyricsComponents.kt
@@ -72,7 +72,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.graphics.drawable.toBitmap
-import androidx.palette.graphics.Palette
+import com.kmpalette.palette.graphics.Palette
 import coil3.ImageLoader
 import coil3.request.ImageRequest
 import coil3.request.allowHardware
