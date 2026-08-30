d1d939b3957ff47baaf387fd942b3fe147239cf0
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-08-30 08:42:47 +0530

    Add Brush import to AutoPlaylistScreen.kt

--- app/src/main/kotlin/com/jay/glossy/ui/screens/playlist/AutoPlaylistScreen.kt
+++ app/src/main/kotlin/com/jay/glossy/ui/screens/playlist/AutoPlaylistScreen.kt
@@ -47,6 +47,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
