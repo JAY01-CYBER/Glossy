5e470b2a685edb6d7ca1765ab6cc365e321dcbfb
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-09-08 09:52:07 +0530

    Add new preference key for Musixmatch integration

--- app/src/main/kotlin/com/jay/glossy/constants/PreferenceKeys.kt
+++ app/src/main/kotlin/com/jay/glossy/constants/PreferenceKeys.kt
@@ -55,7 +55,7 @@ enum class DensityScale(
 
 val DefaultOpenTabKey = stringPreferencesKey("defaultOpenTab")
 val SlimNavBarKey = booleanPreferencesKey("slimNavBar")
-val UseFloatingNavBarKey = booleanPreferencesKey("useFloatingNavBar") // NEW FLOATING NAV BAR KEY
+val UseFloatingNavBarKey = booleanPreferencesKey("useFloatingNavBar") 
 val GridItemsSizeKey = stringPreferencesKey("gridItemSize")
 val SliderStyleKey = stringPreferencesKey("sliderStyle")
 val SquigglySliderKey = booleanPreferencesKey("squigglySlider")
@@ -103,6 +103,7 @@ val EnableLrcLibKey = booleanPreferencesKey("enableLrclib")
 val EnableBetterLyricsKey = booleanPreferencesKey("enableBetterLyrics")
 val EnablePaxsenixKey = booleanPreferencesKey("enablePaxsenix")
 val EnableLyricsPlus = booleanPreferencesKey("enableLyricsPlus")
+val EnableMusixmatchKey = booleanPreferencesKey("enableMusixmatch") 
 val HideExplicitKey = booleanPreferencesKey("hideExplicit")
 val HideVideoSongsKey = booleanPreferencesKey("hideVideoSongs")
 val HideYoutubeShortsKey = booleanPreferencesKey("hideYoutubeShorts")
@@ -438,7 +439,8 @@ enum class PreferredLyricsProvider {
     KUGOU,
     BETTER_LYRICS,
     PAXSENIX,
-    LYRICSPLUS
+    LYRICSPLUS,
+    MUSIXMATCH // Musixmatch Enum
 }
 
 enum class PlayerButtonsStyle {
