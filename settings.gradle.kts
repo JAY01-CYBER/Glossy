ced00ad5a75620f201506d736de06cd964f0e8a5
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-09-08 14:19:37 +0530

    Add youlyplus module to settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,6 +35,8 @@ include(":betterlyrics")
 include(":shazamkit")
 include(":paxsenix")
 include(":musixmatch")
+include(":youlyplus")
+
 
 // Use a local copy of NewPipe Extractor by uncommenting the lines below.
 // We assume, that Metrolist and NewPipe Extractor have the same parent directory.
