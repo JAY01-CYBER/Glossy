7e4ea26a37cab79630960ecf7dab3997a3e1e5fe
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-09-09 07:53:06 +0530

    Add dependency for syrics project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -392,6 +392,7 @@ dependencies {
     implementation(project(":youlyplus"))
     implementation(project(":unison"))
     implementation(project(":simpmusic"))
+    implementation(project(":syrics"))
 
 
     implementation(libs.ktor.client.core)
