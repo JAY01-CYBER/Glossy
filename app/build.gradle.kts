939e223b905f0db91feada386821ec79d94c44db
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-09-09 15:51:45 +0530

    Update version code and name to 3 and 0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = applicationIdOverride ?: baseApplicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
         resValue("string", "app_name", appNameOverride ?: "Glossy")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
