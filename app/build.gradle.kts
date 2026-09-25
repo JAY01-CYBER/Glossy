46c85669b7edaf9c46440b7194d4dc6c9e45508d
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-09-25 17:07:24 +0530

    Bump version code and name to 4 and 0.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,8 +100,8 @@ android {
         applicationId = applicationIdOverride ?: baseApplicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
         resValue("string", "app_name", appNameOverride ?: "Glossy")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
